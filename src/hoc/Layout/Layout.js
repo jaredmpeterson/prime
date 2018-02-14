71dadc4762e9b4a04774412e776c81f41c470799
Jared M. Peterson <developer@peteyinc.com>
2018-02-14 11:07:02 -0700

    Layout hoc

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Aux from "../Aux/Aux";
+
+class Layout extends Component {
+  state = {};
+  render() {
+    return <Aux />;
+  }
+}
+
+export default Layout;
