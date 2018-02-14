a2d58683cdc928856b9087aa0e4db7b163b86ab9
Jared M. Peterson <developer@peteyinc.com>
2018-02-14 11:07:30 -0700

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import logo from "./Scale.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="Prime Scale" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
