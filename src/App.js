c1af0f911c61a34b62f954330964b635102cdbe9
Jared M. Peterson <developer@peteyinc.com>
2018-02-15 11:35:28 -0700

    add Diet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Layout from "./hoc/Layout/Layout";
 import Landing from "./components/UI/Landing/Landing";
+import Info from "./containers/Info/Info";
+import Diet from "./containers/Diet/Diet";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="Prime Scale" />
-          </header> */}
-          <Landing />
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/info" component={Info} />
+            <Route path="/diet" component={Diet} />
+          </Switch>
         </Layout>
       </div>
     );
